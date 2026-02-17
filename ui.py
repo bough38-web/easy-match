6efe8d2b83b177331311dd4189d908a019b3e717
heebonpark <bough38@gmail.net>
2026-02-17 14:55:28 +0900

    feat: Refine Integrated Expert Filters (v1.0.13)

--- ui.py
+++ ui.py
@@ -598,9 +598,8 @@ class MultiFilterRow:
         self.cb_col.bind("<<ComboboxSelected>>", self._on_col_change)
         
         # Operator
-        self.cb_op = ttk.Combobox(self.frame, textvariable=self.op_var, values=["==", ">=", "<=", ">", "<", "Exist", "Not Exist"], state="readonly", width=8)
+        self.cb_op = ttk.Combobox(self.frame, textvariable=self.op_var, values=["==", ">=", "<=", ">", "<"], state="readonly", width=4)
         self.cb_op.pack(side="left", padx=(0, 5))
-        self.cb_op.bind("<<ComboboxSelected>>", self._on_op_change)
         
         # Value
         self.cb_val = ttk.Combobox(self.frame, textvariable=self.val_var, state="normal", width=12)
@@ -619,7 +618,9 @@ class MultiFilterRow:
             self.cb_val.state(["!disabled"])
             self.btn_load.state(["!disabled"])
         
-        # Remove
+        self.refresh_cols()
+
+    def refresh_cols(self):
         btn_rem = ttk.Button(self.frame, text="X", width=2, command=lambda: on_remove(self))
         btn_rem.pack(side="left")
 
